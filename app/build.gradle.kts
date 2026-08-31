33e840182fbc6c85145592419da2d68fca322ab9
0xdeadf1sh <0xdeadf1sh@tuta.io>
2026-08-31 17:28:00 +0400

    Bump to 0.52.0 (versionCode 123).

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
 
     defaultConfig {
         applicationId = "com.t1dm.app"
-        versionCode = 122
-        versionName = "0.51.7"
+        versionCode = 123
+        versionName = "0.52.0"
 
         // Single target device.
         ndk {
