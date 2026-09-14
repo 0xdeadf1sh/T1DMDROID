9331d00bbdfcdd138507a315fa273d9759aaf9ca
0xdeadf1sh <0xdeadf1sh@tuta.io>
2026-09-14 19:52:38 +0400

    Bump to 0.72.5 (versionCode 189)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
 
     defaultConfig {
         applicationId = "com.t1dm.app"
-        versionCode = 171
-        versionName = "0.68.2"
+        versionCode = 189
+        versionName = "0.72.5"
 
         // Single target device.
         ndk {
